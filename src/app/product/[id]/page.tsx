655330f80d3b32e29eb9cd74d80cd54bede44a76
Victor <soujvnunes@gmail.com>
2025-02-26 10:42:45 -0300

    feat(app/product/:id) implement back to previous page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,18 @@
 import { type NextParams } from 'next'
 import { getProduct } from '@/actions/getProduct'
+import PreviousPage from '@/components/PreviousPage'
 
 interface ProductPageProps {
   params: NextParams<'id'>
 }
+
 export default async function ProductPage({ params }: ProductPageProps) {
   const id = Number((await params).id)
   const product = await getProduct(id)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <PreviousPage />
       {product.title}
     </div>
   )
