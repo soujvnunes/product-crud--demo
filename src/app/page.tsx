42d24d9c564d52ecf7c5d037af79c0b38295a9fd
Victor <soujvnunes@gmail.com>
2025-02-26 10:05:51 -0300

    feat(app/page) implement pagination using search params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,49 @@
 import { getProducts } from '@/actions/getProducts'
+import { type NextSearchParams } from 'next'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-export default async function Home() {
-  const products = await getProducts()
+interface HomeProps {
+  searchParams: NextSearchParams<'page'>
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const page = Number((await searchParams).page)
+
+  // If there's any search parameter (page === NaN | 0), redirects to the page with default search param (1)
+  if (!page) redirect(`/?page=1`)
+
+  const products = await getProducts(page)
+  const hasPrevious = page - 1 > 0
 
   return (
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <ul className="grid grid-cols-3 justify-items-center">
-        {products.map((product) => (
+        {products.data.map((product) => (
           <li key={product.id}>
             {product.id}
             <Link href={`/product/${product.id}`}>Buy</Link>
           </li>
         ))}
       </ul>
+      <Link
+        aria-disabled={!hasPrevious || undefined}
+        className={!hasPrevious ? 'pointer-events-none' : ''}
+        href={{
+          pathname: '/',
+          query: { page: page - 1 },
+        }}>
+        Previous
+      </Link>
+      <Link
+        aria-disabled={!products.hasNext || undefined}
+        className={!products.hasNext ? 'pointer-events-none' : ''}
+        href={{
+          pathname: '/',
+          query: { page: page + 1 },
+        }}>
+        Next
+      </Link>
     </div>
   )
 }
