9419be6dae86652452ed67887dbabe58d3732a44
Victor <soujvnunes@gmail.com>
2025-02-27 16:36:32 -0300

    feat(components/ProductsCategorySelectClient) implement useEnhancedSearchParams

--- src/components/ProductsCategorySelectClient.tsx
+++ src/components/ProductsCategorySelectClient.tsx
@@ -1,13 +1,8 @@
 'use client'
 
 import { productsPage } from '@/consts/dict'
-import {
-  ALL_CATEGORY,
-  type AllProductCategory,
-} from '@/helpers/getAllCategories'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useCallback } from 'react'
 import { Select } from './ui/Select'
+import useEnhancedSearchParams from '@/hooks/useEnhancedSearchParams'
 
 interface ProductsCategorySelectClientProps {
   children: React.ReactNode
@@ -16,41 +11,19 @@ interface ProductsCategorySelectClientProps {
 
 export default function ProductsCategorySelectClient({
   category,
+  children,
   ...props
 }: ProductsCategorySelectClientProps) {
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const handleValueChange = useCallback(
-    (value: AllProductCategory) => {
-      const params = new URLSearchParams(searchParams.toString())
-      const currentCategory = params.get(productsPage.searchParams.category)
-      const isAllCategory = value === ALL_CATEGORY
-
-      // I don't want to trigger a rerender on the search params
-      // if user clicks on 'all' without any category listed.
-      if (!currentCategory && isAllCategory) return
-
-      let newParams = ''
-
-      if (isAllCategory) {
-        params.delete(productsPage.searchParams.category)
-        newParams = ''
-      } else {
-        params.set(productsPage.searchParams.category, value)
-        newParams = `?${params.toString()}`
-      }
-
-      router.push(pathname + newParams)
-    },
-    [pathname, router, searchParams],
+  const searchParams = useEnhancedSearchParams(
+    productsPage.searchParams.category,
   )
 
   return (
     <Select
-      onValueChange={handleValueChange}
-      value={category}
-      {...props}
-    />
+      value={category || ''}
+      onValueChange={searchParams.change}
+      {...props}>
+      {children}
+    </Select>
   )
 }
