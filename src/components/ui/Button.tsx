ff37f7b36089ffab38062255df1053e04d9a169c
Victor <soujvnunes@gmail.com>
2025-02-27 18:36:44 -0300

    feat(compoents/ui/Button) adjust [size=sm] classes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -38,7 +38,7 @@ export const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
+        sm: 'h-6 rounded-sm px-2 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'w-9 h-9',
       },
