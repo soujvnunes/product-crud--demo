5bcf66b311c0665113c61675e27467d98cae4a88
Victor <soujvnunes@gmail.com>
2025-02-28 17:32:07 -0300

    feat(jest.config) setup jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest'
+
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default createJestConfig(config)
