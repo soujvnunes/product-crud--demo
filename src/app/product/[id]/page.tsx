f6e052e3a905d5a1e6b8b6a9ef9f722eb4416638
Victor <soujvnunes@gmail.com>
2025-02-28 11:24:49 -0300

    feat(app/product/:id) implement product remove and product details ui

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,28 +1,81 @@
 import { type NextParams } from 'next'
 import { getProduct } from '@/actions/getProduct'
-import PreviousPage from '@/components/PreviousPage'
 import { getProducts } from '@/actions/getProducts'
-import { productPage } from '@/consts/dict'
+import { productPage, productsPage } from '@/consts/dict'
+import { type ProductsPageProps } from '@/app/page'
+import ProductImage from '@/components/ProductImage'
+import LinkButton from '@/components/ui/LinkButton'
+import { ArrowLeft } from 'lucide-react'
+import ProductRemove from '@/components/ProductRemove'
 
-interface ProductPageProps {
+interface ProductPageProps extends ProductsPageProps {
   params: NextParams<typeof productPage.params.id>
 }
 
 export async function generateStaticParams() {
   // TODO: get page using cookies
-  const products = await getProducts(1)
+  const products = await getProducts({ page: 1 })
 
-  return products.data.map(({ id }) => ({ id: String(id) }))
+  return products.data.map((product) => ({ id: String(product.id) }))
 }
 
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductPage({
+  params,
+  searchParams,
+}: ProductPageProps) {
+  const { page, ...queries } = await searchParams
   const id = Number((await params)[productPage.params.id])
   const product = await getProduct(id)
 
+  const sanitizedDescription = product.description.split(/\. +/)
+  const href = {
+    pathname: productsPage.pathname,
+    query: {
+      [productsPage.searchParams.page]: Number(page),
+      ...queries,
+    },
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <PreviousPage />
-      {product.title}
-    </div>
+    <main className="max-w-5xl px-4 lg:px-8 mx-auto">
+      <header className="flex h-16 items-center">
+        <LinkButton
+          variant="ghost"
+          href={href}>
+          <ArrowLeft />
+          Back
+        </LinkButton>
+        <h2 className="font-semibold truncate mr-2">{product.title}</h2>
+        <ProductRemove
+          href={href}
+          value={id}
+        />
+      </header>
+      <div className="grid lg:grid-cols-2 gap-4">
+        <ProductImage
+          sizes="(min-width: 808px) 50vw, 100vw"
+          src={product.image}
+        />
+        <div className="space-y-4">
+          <p>
+            <strong className="text-xs block uppercase mb-2">Title</strong>
+            {product.title}
+          </p>
+          <p className="space-y-2">
+            <strong className="text-xs block uppercase mb-2">
+              Description
+            </strong>
+            {sanitizedDescription.map((sentence, index) => (
+              <span
+                className="block"
+                key={sentence}>
+                {sentence}
+                {sanitizedDescription.length - 1 !== index && '.'}
+              </span>
+            ))}
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
